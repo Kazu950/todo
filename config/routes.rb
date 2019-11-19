261795e83a2b8314919a292615ee78bd6933ea0f
Kazu950 <kazuki.basket.617-40@docomo.ne.jp>
2019-11-19 22:58:04 +0900

    routeの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'todo_lists/index'
-  get 'todo_lists/show'
-  get 'todo_lists/new'
-  get 'todo_lists/edit'
-  get 'index/show'
-  get 'index/new'
-  get 'index/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :todo_lists
+  root to: 'todo_lists#index'
 end
