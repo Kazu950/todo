3c786f85c71db448a1eddc214b23678c122a4841
Kazu950 <kazuki.basket.617-40@docomo.ne.jp>
2019-11-19 22:34:48 +0900

    コントローラ修正

---
+++ app/controllers/todo_lists_controller.rb
@@ -0,0 +1,13 @@
+class TodoListsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'todo_lists/index'
+  get 'todo_lists/show'
+  get 'todo_lists/new'
+  get 'todo_lists/edit'
   get 'index/show'
   get 'index/new'
   get 'index/edit'
