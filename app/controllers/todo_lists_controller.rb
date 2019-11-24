6182a8891b7b1260364d99fc3e3bbc89422b6e40
Kazu950 <kazuki.basket.617-40@docomo.ne.jp>
2019-11-24 17:21:18 +0900

    新規登録機能実装

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,13 +1,27 @@
 class TodoListsController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save!
+    redirect_to todo_lists_path, notice: "#{@task.name}"
+  end
+
+  private
+  
+  def task_params
+    params.require(:task).permit(:name, :description, :priority)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :todo_lists
   root to: 'todo_lists#index'
+  post 'todo_lists/new' => 'todo_lists#create'
 end
